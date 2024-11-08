16cdc3426a78b36ae38bb000d1e255251a1b3eb1
Phap Man <aaron.kyle.solomon@gmail.com>
2024-11-08 15:21:56 -0800

    create package for clean_parse_tag functionality

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tnh-scholar',
+    version='0.1',
+    packages=find_packages(include=["tnh_process"]),
+    install_requires=[],  # Add any dependencies here
+)
