0ce5eaafbcc9484f8004c547f330452f80913b28
Phap Man <aaron.kyle.solomon@gmail.com>
2024-11-13 08:15:08 -0800

    adjust setup for new packages and fixes for making packages appear.

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='tnh-scholar',
     version='0.1',
-    packages=find_packages(include=["tnh_process"]),
+    packages=find_packages(include=['data_processing', 'data_processing.*']),
     install_requires=[],  # Add any dependencies here
 )
