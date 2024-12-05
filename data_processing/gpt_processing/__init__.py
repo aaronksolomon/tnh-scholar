32242e1e78efac2ec3bdc2af7c55a496130c6399
Phap Man <aaron.kyle.solomon@gmail.com>
2024-12-05 13:39:12 -0800

    include only functions that are intended to be user accessible

--- data_processing/gpt_processing/__init__.py
+++ data_processing/gpt_processing/__init__.py
@@ -1,1 +1,14 @@
-from .gpt_interface import *
+from .gpt_interface import (
+    token_count, get_api_client, set_model_settings, get_model_settings,
+    generate_messages, run_immediate_chat_process, get_completion_content,
+    create_jsonl_file_for_batch, poll_batch_for_response, 
+    start_batch_with_retries,
+    get_active_batches,
+    get_batch_status,
+    get_completed_batches,
+    get_all_batch_info,
+    get_batch_response,
+    get_last_batch_response,
+    run_single_batch,
+    delete_api_files,
+)
