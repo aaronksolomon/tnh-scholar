325255843d03f05dd409b784302a790996f2279b
Phap Man <aaron.kyle.solomon@gmail.com>
2024-12-12 03:39:39 -0800

    new access functions for running immediate batches and completions. restructuring api client access. format file.

--- data_processing/gpt_processing/__init__.py
+++ data_processing/gpt_processing/__init__.py
@@ -1,7 +1,14 @@
 from .gpt_interface import (
-    token_count, get_api_client, set_model_settings, get_model_settings,
-    generate_messages, run_immediate_chat_process, get_completion_content,
-    create_jsonl_file_for_batch, poll_batch_for_response, 
+    token_count, 
+    get_api_client, 
+    set_model_settings, 
+    get_model_settings,
+    generate_messages, 
+    run_immediate_chat_process, 
+    get_completion_content,
+    get_completion_object,
+    create_jsonl_file_for_batch, 
+    poll_batch_for_response, 
     start_batch_with_retries,
     get_active_batches,
     get_batch_status,
@@ -11,4 +18,6 @@ from .gpt_interface import (
     get_last_batch_response,
     run_single_batch,
     delete_api_files,
+    run_immediate_completion_simple,
+    run_transcription_speech
 )
