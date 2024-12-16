8e2c3cda0f90b0fc678a95e0a84877cb65e30700
Phap Man <aaron.kyle.solomon@gmail.com>
2024-12-16 12:36:41 -0800

    add CLI entry point for audio_transcribe

--- setup.py
+++ setup.py
@@ -5,4 +5,9 @@ setup(
     version='0.1',
     packages=find_packages(include=['data_processing', 'data_processing.*']),
     install_requires=[],  # Add any dependencies here
+    entry_points={
+        "console_scripts": [
+            "audio-transcribe=CLI_tools.audio_transcribe.audio_transcribe:main",
+        ],
+    },
 )
