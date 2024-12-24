6365e38890daa5e5b08e39f612bb615f0b2d5ff8
Phap Man <aaron.kyle.solomon@gmail.com>
2024-12-24 11:09:47 -0800

    change project wide variable names to be more clear.

--- src/tnh_scholar/__init__.py
+++ src/tnh_scholar/__init__.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
 # Dynamically determine and set up paths for the project
-TNH_BASE_DIR = Path(__file__).resolve().parent
-PROJECT_ROOT_DIR = TNH_BASE_DIR.resolve().parent.parent # always assume structure is: root_dir/src/TNH_BASE_DIR
-CLI_TOOLS_DIR = TNH_BASE_DIR / "CLI_tools"
+TNH_ROOT_SRC_DIR = Path(__file__).resolve().parent
+PROJECT_ROOT_DIR = TNH_ROOT_SRC_DIR.resolve().parent.parent # always assume structure is: root_dir/src/TNH_BASE_DIR
+CLI_TOOLS_DIR = TNH_ROOT_SRC_DIR / "CLI_tools"
 LOG_DIR = PROJECT_ROOT_DIR / "logs"
 
-if not TNH_BASE_DIR.exists():
-    raise FileNotFoundError(f"Base directory {TNH_BASE_DIR} does not exist.")
+if not TNH_ROOT_SRC_DIR.exists():
+    raise FileNotFoundError(f"Base directory {TNH_ROOT_SRC_DIR} does not exist.")
 if not CLI_TOOLS_DIR.exists():
     raise FileNotFoundError(f"CLI tools directory {CLI_TOOLS_DIR} does not exist.")
