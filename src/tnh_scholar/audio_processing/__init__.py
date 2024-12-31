d63f6a394dfaa98904bd669194927dd39491062e
Phap Man <aaron.kyle.solomon@gmail.com>
2024-12-31 10:23:09 -0800

    remove silence_percentage.

--- src/tnh_scholar/audio_processing/__init__.py
+++ src/tnh_scholar/audio_processing/__init__.py
@@ -2,11 +2,10 @@ from .audio_processing import (
     split_audio,
     detect_whisper_boundaries,
     detect_nonsilent,
-    split_audio_at_boundaries,
-    calculate_silence_percentage
+    split_audio_at_boundaries
 )
 
-from .audio_transcription import (
+from .transcription import (
     process_audio_file,
     process_audio_chunks
 )
