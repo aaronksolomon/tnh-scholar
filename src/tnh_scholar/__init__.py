120bf519c227e76e9ab3bbc64517cb6fae8a96bb
Phap Man <aaron.kyle.solomon@gmail.com>
2025-01-10 06:11:19 -0800

    update directory name to lowercase cli_tools.

--- src/tnh_scholar/__init__.py
+++ src/tnh_scholar/__init__.py
@@ -3,8 +3,9 @@ from pathlib import Path
 # Dynamically determine and set up paths for the project
 TNH_ROOT_SRC_DIR = Path(__file__).resolve().parent
 PROJECT_ROOT_DIR = TNH_ROOT_SRC_DIR.resolve().parent.parent # always assume structure is: root_dir/src/TNH_BASE_DIR
-CLI_TOOLS_DIR = TNH_ROOT_SRC_DIR / "CLI_tools"
+CLI_TOOLS_DIR = TNH_ROOT_SRC_DIR / "cli_tools"
 LOG_DIR = PROJECT_ROOT_DIR / "logs"
+PATTERN_REPO = PROJECT_ROOT_DIR / "pattern_repo" 
 
 if not TNH_ROOT_SRC_DIR.exists():
     raise FileNotFoundError(f"Base directory {TNH_ROOT_SRC_DIR} does not exist.")
